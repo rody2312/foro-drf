0c0692835f9a3b24e53c2b04d8ac17846ba79453
rody2312 <84883025+rody2312@users.noreply.github.com>
2023-07-18 00:00:19 -0400

    modelos blog y comments

--- backend/blogs/models.py
+++ backend/blogs/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+class Blog(models.Model):
+    body = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    date = models.DateField(auto_now_add=True)
+
+
+class Comments(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    text = models.CharField(max_length=100)
+    date = models.DateTimeField(auto_now_add=True)
